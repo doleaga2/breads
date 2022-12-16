bd3b91c2028c9bf88a9ada3468d9c01dc26b1708
doleaga2 <oleaga.diana@gmail.com>
2022-12-15 19:33:17 -0500

    making image show

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -5,10 +5,11 @@ const Bread = require('../models/bread.js')
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
-    res.send(Bread[req.params.arrayIndex])
-  })
-  
-  
-
+    
+    //res.send(`<img src='Bread[req.params.arrayIndex].image'></img>`)
+    let breadimg = Bread[req.params.arrayIndex].image
+    //res.send(Bread[req.params.arrayIndex])
+    res.send(`<img src=${breadimg}></img>`)
+})
 module.exports = breads
 
