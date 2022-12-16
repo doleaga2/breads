ef5f4d94097e5ef8b6ce9640653f53002c170341
doleaga2 <oleaga.diana@gmail.com>
2022-12-15 19:39:02 -0500

    small updates

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -6,9 +6,9 @@ const Bread = require('../models/bread.js')
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
     
-    //res.send(`<img src='Bread[req.params.arrayIndex].image'></img>`)
+   
     let breadimg = Bread[req.params.arrayIndex].image
-    //res.send(Bread[req.params.arrayIndex])
+    
     res.send(`<img src=${breadimg}></img>`)
 })
 module.exports = breads
