682b9c9425e771e19264a661623cbcfb7f6b513a
doleaga2 <oleaga.diana@gmail.com>
2022-12-15 20:11:17 -0500

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
+// INDEX
+breads.get('/', (req, res) => {
+    res.render('Index',
+      {
+        breads: Bread
+      }
+    )
+  // res.send(Bread)
+})
 
 
 // SHOW
